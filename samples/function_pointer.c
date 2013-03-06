d95f586faf7fa10f13fc9b072248604b4358a51f
e115763 <e115763@gmail.com>
2013-03-06 20:43:29 +0900

    add function_pointer in C

---
+++ samples/function_pointer.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define arr_length 5
+
+void block(int);
+void each(int[], void (*)(int));
+
+int main(int argc, char const* argv[]) {
+    int arr[arr_length] = {1, 1, 2, 3, 5};
+
+    each(arr, block);
+    
+    return 0;
+}
+
+void block(int item) {
+    printf("%d\n", item);
+}
+
+void each(int arr[], void (*block)(int)) {
+    int i;
+    for (i = 0; i < arr_length; i++) {
+        block(arr[i]);
+    }
+}
