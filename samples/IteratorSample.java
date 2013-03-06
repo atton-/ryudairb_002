1263b716fbe96aac84acc7ba3370da41725eb7a7
e115763 <e115763@gmail.com>
2013-03-06 20:20:38 +0900

    add iterator sample in Java

---
+++ samples/IteratorSample.java
@@ -0,0 +1,8 @@
+public class IteratorSample {
+    public static void main(String args[]) {
+        int[] arr = {1, 1, 2, 3, 5};
+        for (int item : arr) {
+            System.out.println(item);
+        }
+    }
+}
