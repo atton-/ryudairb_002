5649b2628accf1ac1e3f6b43afabd0eabfc14671
e115763 <e115763@gmail.com>
2013-03-07 22:25:09 +0900

    support empty list in lambda.scm

--- samples/lambda.scm
+++ samples/lambda.scm
@@ -1,10 +1,7 @@
 (define (each arr block)
-  (let ((item  (car arr))
-        (items (cdr arr)))
-    (if (null? items)
-      (block item)
-      (begin (block item)
-             (each items block)))))
+    (if (not (null? arr))
+      (begin (block (car arr))
+             (each (cdr arr) block))))
 
 (define arr '(1 1 2 3 5))
 (define block (lambda (item) (begin (display item) (newline))))
